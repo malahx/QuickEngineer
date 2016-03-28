65af477abc9ba21850747449fbaf795701938f21
malah <malah@neuf.fr>
2016-03-28 17:48:28 +0200

    v0.1beta

---
+++ VesselSimulator/KerbalEngineer/KerbalEngineer/LogMsg.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+using UnityEngine;
+
+namespace KerbalEngineer
+{
+    public class LogMsg
+    {
+        public StringBuilder buf;
+
+        public LogMsg()
+        {
+            this.buf = new StringBuilder();
+        }
+
+        public void Flush()
+        {
+            if (this.buf.Length > 0)
+                MonoBehaviour.print(this.buf);
+            this.buf.Length = 0;
+        }
+    }
+}
